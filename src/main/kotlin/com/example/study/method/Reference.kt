5f3bfa1059fa3dfb9b98e368493f062082306929
sigenogoro <25719873+sigenogoro@users.noreply.github.com>
2023-07-25 23:59:39 +0900

    aaaaa

---
+++ src/main/kotlin/com/example/study/method/Reference.kt
@@ -0,0 +1,11 @@
+package com.example.study.method
+
+fun evenNumber(number: Int) = number % 2 == 0
+
+
+fun main(){
+    val listNumber = listOf(0,5,4,2,7)
+
+    val evenNumber = listNumber
+    println(evenNumber)
+}
