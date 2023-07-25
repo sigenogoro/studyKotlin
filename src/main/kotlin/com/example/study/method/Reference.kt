9c06a41dac3fc337de8f7bbe859c27f3fa0bf134
sigenogoro <25719873+sigenogoro@users.noreply.github.com>
2023-07-26 00:02:52 +0900

    メソッド参照を学習

--- src/main/kotlin/com/example/study/method/Reference.kt
+++ src/main/kotlin/com/example/study/method/Reference.kt
@@ -1,11 +1,14 @@
 package com.example.study.method
 
-fun evenNumber(number: Int) = number % 2 == 0
+fun evenNumber(number: Int): Boolean {
+    return number % 2 == 0
+}
 
 
-fun main(){
-    val listNumber = listOf(0,5,4,2,7)
+fun main() {
+    val listNumber = listOf(0, 5, 4, 2, 7)
 
-    val evenNumber = listNumber
-    println(evenNumber)
+    val result = listNumber.filter(::evenNumber)
+    // 偶数である値を出力する
+    println(result)
 }
