cec57c0d15f600e6284eb35a7efa197ff6fbae0c
sigenogoro <25719873+sigenogoro@users.noreply.github.com>
2023-08-16 23:34:16 +0900

    変数の使い方

---
+++ src/main/kotlin/com/example/study/classA/SampleCode.kt
@@ -0,0 +1,11 @@
+package com.example.study.classA
+
+fun main() {
+    // 変数名に型を定義しなくても入れることはできる
+
+//  IntelliJ からは、警告文として「冗長である」ことが記載されている
+    val message: String = "Hello World"
+    val age = 11
+    println(message)
+    println(age)
+}
